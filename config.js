ddf3bbdf749cafa69cc5d73424840413b986b141
Ezra Keddell <ezrakeddell@gmail.com>
2016-06-08 13:37:28 +1200

    hodor

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key: CONSUMER_KEY,
+  consumer_secret: CONSUMER_SECRET,
+  access_token: ACCESS_TOKEN,
+  access_token_secret: ACCESS_TOKEN_SECRET
+};
