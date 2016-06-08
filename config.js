3764ef09a4659a567181c45d4414b5c7967bafcf
Ezra Keddell <ezrakeddell@gmail.com>
2016-06-08 13:13:00 +1200

    Delete config

--- config.js
+++
@@ -1,7 +0,0 @@
-// Put your own Twitter App keys here. See README.md for more detail.
-module.exports = {
-  consumer_key: 'J8BwD90dEu7l9wTzTPKvXPkoF',
-  consumer_secret: 'LiamcyNYIEBy7UM3OLPulJnGcupPPUla6JIqoxOBHYweZFgiAo',
-  access_token: '735688117739343874-au0drj8JNdEUxLnVnRp0STJgmQ9Hjgm',
-  access_token_secret: 'TTqRnpppZhaupqh368j9gLTwYzDUIYGR8mrIUuZy0JCrG'
-};
